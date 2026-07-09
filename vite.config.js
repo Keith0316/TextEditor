ec0fbc9d8e39a23233d342cb654debdee94e96be
hp3306 <1554938485@qq.com>
2026-07-09 16:13:03 +0800

    配置 GitHub Pages 自动部署 + 修正基础路径

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true
-  }
+  // 格式：/你的仓库名/，前后都必须有斜杠
+  base: '/TextEditor/',
 })
